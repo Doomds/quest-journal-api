bef0e32193ceb3b33b0f5db8d2d8c530be8ee003
Doom <de.simone.dc@gmail.com>
2025-11-22 12:41:20 +0100

    added caracteristique entity

---
+++ src/test/java/com/quest/journal/entity/Caracteristique.java
@@ -0,0 +1,35 @@
+package com.quest.journal.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "caracteristique")
+public class Caracteristique {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private char type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+}
